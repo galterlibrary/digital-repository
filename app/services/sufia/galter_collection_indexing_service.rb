dada435a10cba6dbd0298026bc888b1502359479
Piotr Hebal <phebal@gmail.com>
2015-11-20 08:24:22 -0600

    Skip label field generation for collections with empty title.

--- app/services/sufia/galter_collection_indexing_service.rb
+++ app/services/sufia/galter_collection_indexing_service.rb
@@ -2,7 +2,11 @@ module Sufia
   class GalterCollectionIndexingService < ActiveFedora::IndexingService
     def generate_solr_document
       super.tap do |solr_doc|
-        solr_doc[Solrizer.solr_name('label', :sortable)] = object.title.downcase
+        if object.title.present?
+          solr_doc[
+            Solrizer.solr_name('label', :sortable)
+          ] = object.title.downcase
+        end
       end
     end
   end
