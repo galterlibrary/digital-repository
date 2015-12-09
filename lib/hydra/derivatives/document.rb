a469a84259a5458bd02041cdc4302c48f782c2b5
Piotr Hebal <phebal@gmail.com>
2015-12-09 08:19:54 -0600

    Fix derivatives generation for some pptx, fixes #304 There was an intermediate step that converted document to pdf before converting it to jpg that would get stuck with CPU pegged at 100% when creating derivatives, removing the intermediate step seemed to fix it.

---
+++ lib/hydra/derivatives/document.rb
@@ -0,0 +1,52 @@
+module Hydra
+  module Derivatives
+    class Document < Processor
+      include ShellBasedProcessor
+
+      def self.encode(path, options, output_file)
+        format = File.extname(output_file).sub('.', '')
+        outdir = File.dirname(output_file)
+        execute "#{Hydra::Derivatives.libreoffice_path} --invisible --headless --convert-to #{format} --outdir #{outdir} #{path}"
+      end
+
+      def encode_file(dest_path, file_suffix, mime_type, options = { })
+        new_output = ''
+        source_file.to_tempfile do |f|
+          self.class.encode(f.path, options, output_file(file_suffix))
+          new_output = File.join(
+            Hydra::Derivatives.temp_file_base, [
+              File.basename(f.path).sub(File.extname(f.path), ''),
+              file_suffix
+            ].join('.')
+          )
+        end
+        out_file = File.open(new_output, "rb")
+        object.add_file(out_file.read, path: dest_path, mime_type: mime_type)
+        File.unlink(out_file)
+      end
+
+      def output_file(file_suffix)
+        Dir::Tmpname.create(["#{object.id.gsub('/', '_')}-content.", ".#{file_suffix}"], Hydra::Derivatives.temp_file_base){}
+      end
+
+      def new_mime_type(format)
+        case format
+        when 'pdf'
+          'application/pdf'
+        when 'odf'
+          'application/vnd.oasis.opendocument.text'
+        when 'docx'
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        when 'xslx'
+          'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        when 'pptx'
+          'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+        when 'jpg'
+          'image/jpeg'
+        else
+          raise "I don't know about the format '#{format}'"
+        end
+      end
+    end
+  end
+end
