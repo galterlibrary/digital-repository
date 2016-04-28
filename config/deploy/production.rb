03992a61fced4201da1be8e4dc6ebb3602c6f53b
Piotr Hebal <phebal@gmail.com>
2016-04-28 08:33:01 -0500

    Add NU idp entityID #21

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :production
 
-set :shib_idp, 'https://FIXME.northwestern.edu/idp/shibboleth'
+set :shib_idp, 'urn:mace:incommon:northwestern.edu'
 set :www_host, 'digitalhub.northwestern.edu'
 set :rails_env, 'production'
 set :ssh_host, %W{#{fetch(:ssh_user)}@vfsmghslrepo01.fsm.northwestern.edu}
