b7d9ad37cd9f1a293c592fdd84991404ea1ad1e1
Piotr Hebal <phebal@gmail.com>
2016-08-03 08:43:20 -0500

    Add ability to upload compressed files #446

--- app/assets/javascripts/files.js
+++ app/assets/javascripts/files.js
@@ -2,6 +2,6 @@ $(function () {
   $('#fileupload').fileupload(
       'option',
       'acceptFileTypes',
-      /(\.|\/)(csv|tsv|ods|odf|xlsx?|pdf|pptx?|tex|dvi|odt|rtf|docx?|txt|tiff?|gif|jpe?g|png)$/i
+      /(\.|\/)(csv|tsv|ods|odf|xlsx?|pdf|pptx?|tex|dvi|odt|rtf|docx?|txt|tiff?|gif|jpe?g|png|zip|gz|tar|7z|bz2?)$/i
   );
 });
