c721cbe34ff8c227a2ec9b9e6fc0bc3cff9aa7c0
Piotr Hebal <phebal@gmail.com>
2016-08-12 08:43:23 -0500

    Allow 2GB file upload from non-cloud based interface, #446

--- app/assets/javascripts/files.js
+++ app/assets/javascripts/files.js
@@ -1,4 +1,8 @@
 $(function () {
+  max_file_size = 2147483648;
+  max_file_size_str = "2GB";
+  max_total_file_size = 4294967296;
+  max_total_file_size_str = "4GB";
   $('#fileupload').fileupload(
       'option',
       'acceptFileTypes',
