2eb3a9212afb020a3b2c0159ec9f794c40833b5f
Austin <sharpattack@users.noreply.github.com>
2021-08-20 11:33:02 -0400

    [#915] Add title and address to UsersList
    Include user's title and address to UsersList. This is to help get a
    better idea of their affiliation. closes #915

--- lib/scripts/users_list.rb
+++ lib/scripts/users_list.rb
@@ -10,14 +10,14 @@ class UsersList
   def populate_user_info
     CSV.open(self.csv_file.path, "w") do |csv|
       # add headers
-      csv << ['Formal Name', 'NetID', 'Email', 'Current Sign In', 'Last Sign In', 'Total Files Deposited', 'Last Deposit Date']
+      csv << ['Formal Name', 'NetID', 'Email', 'Title', 'Address', 'Current Sign In', 'Last Sign In', 'Total Files Deposited', 'Last Deposit Date']
 
       select_all_users.find_in_batches do |users_batch|
         users_batch.each do |user|
           solr_results = solr_query_by_depositor(user.username).sort_by { |result| result["date_uploaded_dtsi"] }
           count = solr_results.count
-          csv << [user.formal_name, user.username, user.email,
-                  user.current_sign_in_at, user.last_sign_in_at,
+          csv << [user.formal_name, user.username, user.email, user.title,
+                  user.address, user.current_sign_in_at, user.last_sign_in_at,
                   count, last_upload_date(solr_results, count)]
         end
       end
@@ -26,7 +26,7 @@ class UsersList
 
   private
   def select_all_users
-    User.all.select(:id, :formal_name, :username, :email,
+    User.all.select(:id, :formal_name, :username, :email, :title, :address,
                     :current_sign_in_at, :last_sign_in_at)
   end
 
