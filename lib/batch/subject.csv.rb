0d6bbba41a1b8552f307d820a7dda3fa5fce8506
Austin <sharpattack@users.noreply.github.com>
2022-07-28 16:45:01 -0400

    PR review suggested changes

--- lib/batch/subject.csv.rb
+++ lib/batch/subject.csv.rb
@@ -1,28 +1,35 @@
 csv = CSV.open('/home/deploy/all_gf_subject.csv', 'wb')
 terms =  GalterGenericFilePresenter.terms + [
-  :depositor, :date_uploaded, :date_modified, :read_access_group, :collection_ids, :id
+  :depositor, :date_uploaded, :date_modified, :read_access_group,
+  :collection_ids, :id, :events
 ]
 
-headers = terms.map(&:to_s).map { |term|
+csv << terms.map(&:to_s).map { |term|
   I18n.translate(:simple_form)[:labels][:generic_file][term] || term.titleize
 }
 
-headers << "Events"
-
-csv << headers
-
 def add_metadata_to_csv(model)
-  ActiveFedora::SolrService.query("has_model_ssim:#{model}", { rows: 99999 }).each.with_index do |gf, idx|
+  query = ActiveFedora::SolrService.query(
+    "has_model_ssim:#{model}", { rows: 99999 }
+  )
+
+  query.each.with_index do |gf, idx|
     gfmod = gf.each.map {|k, v| [k.gsub(/_[a-z]+\z/, ''), v] }.to_h
-    values = terms.map {|term|
+
+    csv << terms.map {|term|
       t = term.to_s
-      (gfmod[t].is_a?(Array) ? gfmod[t].reject(&:blank?).compact.join(' ; ') : gfmod[t]).to_s
+
+      if t == "events"
+        events = model.constantize.find(gfmod["id"]).events
+        events.first.blank? ? "no event" : events.first[:action]
+      elsif gfmod[t].is_a?(Array)
+        gfmod[t].reject(&:blank?).compact.join(' ; ')
+      else
+        gfmod[t].to_s
+      end
     }
-    events = model.constantize.find(gfmod["id"]).events
-    event = events.first.blank? ? "no event" : events.first[:action]
-    values << event
-    csv << values
-    puts "Index: #{idx}" if idx % 100 == 0
+
+    puts "#{model} Index: #{idx}" if idx % 100 == 0
   end
 end
 
