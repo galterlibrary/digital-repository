832a0c10bf9c93323f404c452e8cc3670e7d50d6
Austin <sharpattack@users.noreply.github.com>
2022-08-15 16:03:17 -0400

    [#1062] Fix Collection IDs
    For subjects.csv export. closes #1062

--- lib/batch/subject.csv.rb
+++ lib/batch/subject.csv.rb
@@ -1,12 +1,15 @@
-csv = CSV.open('/home/deploy/all_gf_subject.csv', 'wb')
-terms =  GalterGenericFilePresenter.terms + [
-  :depositor, :date_uploaded, :date_modified, :read_access_group,
-  :collection_ids, :id, :events
+@csv = CSV.open('/home/deploy/all_gf_subject.csv', 'wb')
+@terms =  GalterGenericFilePresenter.terms + [
+  :depositor, :date_uploaded, :date_modified, :read_access_group, :id
 ]
 
-csv << terms.map(&:to_s).map { |term|
+header = @terms.map(&:to_s).map { |term|
   I18n.translate(:simple_form)[:labels][:generic_file][term] || term.titleize
 }
+
+header += ["Events", "Collection Ids"]
+
+@csv << header
 
 def add_metadata_to_csv(model)
   query = ActiveFedora::SolrService.query(
@@ -16,18 +19,22 @@ def add_metadata_to_csv(model)
   query.each.with_index do |gf, idx|
     gfmod = gf.each.map {|k, v| [k.gsub(/_[a-z]+\z/, ''), v] }.to_h
 
-    csv << terms.map {|term|
+    row = @terms.map {|term|
       t = term.to_s
 
-      if t == "events"
-        events = model.constantize.find(gfmod["id"]).events
-        events.first.blank? ? "no event" : events.first[:action]
-      elsif gfmod[t].is_a?(Array)
+      if gfmod[t].is_a?(Array)
         gfmod[t].reject(&:blank?).compact.join(' ; ')
       else
         gfmod[t].to_s
       end
     }
+
+    record = model.constantize.find(gfmod["id"])
+    event = record.events.first.blank? ? "no event" : record.events.first[:action]
+    row << event
+    row << record.collection_ids.reject(&:blank?).compact.join(' ; ')
+
+    @csv << row
 
     puts "#{model} Index: #{idx}" if idx % 100 == 0
   end
@@ -36,4 +43,4 @@ end
 add_metadata_to_csv("GenericFile")
 add_metadata_to_csv("Page")
 
-csv.close
+@csv.close
