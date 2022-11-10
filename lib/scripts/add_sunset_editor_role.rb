ab4f83007efc909ab13e62c1757bdcef1287b438
Eric Newman <newmaneric5@gmail.com>
2022-11-10 11:24:37 -0600

    [#1117] Disable File Editing For All Except Specific Users
    Remove buttons that can change collections and generic files from all
    views. closes #1133

---
+++ lib/scripts/add_sunset_editor_role.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# Checks the error rate of paths created based on a GenericFile's checksum.
+# To run from the base directory of this app:
+# `$ b rails r lib/scripts/add_sunset_editor_role.rb`
+
+# Create new role
+Role.create(name: "sunset-editor", description: "Allow user to edit files after DigitalHub is closed to the public")
+
+# Find and assign users to role
+user_emails = ["gretchen.neidhardt@northwestern.edu"]
+users = User.where(net_ids: user_net_ids)
+
+users.each do |user|
+  user.roles << sunset_editor
+end
